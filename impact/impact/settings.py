7277e7c1db9f9e8232d52f3f7abd2b27b4defcc0
emillumine <camille@yaal.coop>
2024-03-15 17:14:20 +0100

    configure whitenoise pour ajouter de la compression et du cache sur les fichiers static tentative pour améliorer les perfs de prod et supprimer les erreurs 503 qui arrivent parfois avec 2 conteneurs web sur la prod

--- impact/impact/settings.py
+++ impact/impact/settings.py
@@ -134,6 +134,14 @@ STATIC_ROOT = Path(BASE_DIR, "static_collected")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (Path(BASE_DIR, "static"),)
 
+# Compression and caching whitenoise configuration to serve static files
+# https://whitenoise.readthedocs.io/en/stable/django.html
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
